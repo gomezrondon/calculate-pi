b77490770918006ea975c8074338651bde49df0c
gomezrondon <gomezrondon@gmail.com>
2020-08-09 10:37:54 -0600

    draw a chrome icon on the canvas

--- src/main/kotlin/com/gomezrondon/image/ImageDetection.kt
+++ src/main/kotlin/com/gomezrondon/image/ImageDetection.kt
@@ -1,13 +1,9 @@
 package com.gomezrondon.image
 
 
-import com.gomezrondon.utils.distMy
-import com.gomezrondon.utils.getCENTER
 import com.gomezrondon.utils.getPixelPosi
 import processing.core.PApplet
-import processing.core.PConstants
 import processing.core.PImage
-import processing.core.PVector
 
 fun main() {
     PApplet.main("com.gomezrondon.image.ImageDetection")
@@ -16,28 +12,41 @@ fun main() {
 
 class ImageDetection: PApplet() {
 
-    lateinit var crow:PImage
+    lateinit var chrome:PImage
 
 
     override fun draw() {
-        // how to place an image on the canvas
-        // image(crow, 0f, 0f)
-
         loadPixels() //before doing anything with pixels
-      //  crow.loadPixels()
-
+        chrome.loadPixels()
         for (x in 0..width) {
             for (y in 0..height-2) { // hay un problema con los 2 ultimos pixeles
                 val cIndex = getPixelPosi(x, y,width)
-                val color = crow.get(x, y)
 
+             //   val g = green(chrome.get(x, y))
+                val get = chrome.get(x, y)
 
-                pixels[cIndex] = color(color)
+                pixels[cIndex] = color(get)
 
             }
 
         }
-        updatePixels() // after working with pixels
+        updatePixels() // after working with pixels*/
+
+
+/*        loadPixels() //before doing anything with pixels
+      //  crow.loadPixels()
+        for (x in 0..width) {
+            for (y in 0..height-2) { // hay un problema con los 2 ultimos pixeles
+                val cIndex = getPixelPosi(x, y,width)
+
+                val g = green(crow.get(x, y))
+
+                pixels[cIndex] = color(0f, g, 0f)
+
+            }
+
+        }
+        updatePixels() // after working with pixels*/
     }
 
 
@@ -52,10 +61,10 @@ class ImageDetection: PApplet() {
 
     // method for setting the size of the window
     override fun settings() {
-        size(858, 536)
+        size(56, 46)
 
        // image = PImage()
-        crow =  loadImage("C:\\temp\\test\\processing-3-kotlin\\src\\main\\kotlin\\com\\gomezrondon\\image\\360.jpg")
+        chrome =  loadImage("C:\\temp\\test\\processing-3-kotlin\\src\\main\\kotlin\\com\\gomezrondon\\image\\test5.png")
 
     }
 
